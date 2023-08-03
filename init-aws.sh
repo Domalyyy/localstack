d6ca056af29b874efd51b136774a5a85504f5343
bohdan domalchuk <bohdan.domalchuk@vitechteam.com>
2023-08-03 18:14:10 +0300

    ci: test

--- init-aws.sh
+++ init-aws.sh
@@ -2,17 +2,17 @@
 
 awslocal s3 mb s3://lambda
 
-awslocal s3 mb s3://local-bucket
-
-awslocal sqs create-queue --queue-name local-queue
-
-awslocal s3api put-bucket-notification-configuration --bucket local-bucket --notification-configuration "{
-    \"QueueConfigurations\": [
-      {
-        \"QueueArn\": \"arn:aws:sqs:us-east-2:000000000000:local-queue\",
-        \"Events\": [
-          \"s3:ObjectCreated:*\"
-        ]
-      }
-    ]
-  }"
+#awslocal s3 mb s3://local-bucket
+#
+#awslocal sqs create-queue --queue-name local-queue
+#
+#awslocal s3api put-bucket-notification-configuration --bucket local-bucket --notification-configuration "{
+#    \"QueueConfigurations\": [
+#      {
+#        \"QueueArn\": \"arn:aws:sqs:us-east-2:000000000000:local-queue\",
+#        \"Events\": [
+#          \"s3:ObjectCreated:*\"
+#        ]
+#      }
+#    ]
+#  }"
